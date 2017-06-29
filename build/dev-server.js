67f93433cbee00a25b221ccb1ca139f3d66592bd
unknown <liyne@ufsoft.com.cn>
2017-06-29 17:18:11 +0800

    first commit

--- build/dev-server.js
+++ build/dev-server.js
@@ -73,6 +73,7 @@ console.log('> Starting dev server...')
 devMiddleware.waitUntilValid(() => {
   console.log('> Listening at ' + uri + '\n')
   // when env is testing, don't need open it
+
   if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
     opn(uri)
   }
