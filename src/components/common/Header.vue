2465a946dfde18c0fb355a240c2904e558b83821
unknown <liyne@ufsoft.com.cn>
2017-07-05 15:07:33 +0800

    update

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -2,7 +2,7 @@
 <div class="header-fixed">
     <x-header :left-options="{showBack: false}">豆瓣电影</x-header>
     <tab :line-width=2>
-               <tab-item selected class="vux-center"><router-link to="/movie">正在热映</router-link></tab-item>
+               <tab-item selected class="vux-center"><router-link to="/movie">电影</router-link></tab-item>
                <tab-item class="vux-center">图书</tab-item>
                <tab-item class="vux-center">广播</tab-item>
                <tab-item class="vux-center">小组</tab-item>
--- src/components/pages/movie.vue
+++ src/components/pages/movie.vue
@@ -1,22 +1,26 @@
 <template>
 	          <div class="tab-swiper vux-center content">
+	                  <div class="weui-search-bar__box">
+	                           <i class="weui-icon-search" v-on:click="searchMovie"></i> 
+	                           <input type="search" v-model="searchText"  autocomplete="off" class="weui-search-bar__input">
+	                            <a href="javascript:" class="weui-icon-clear" style="display: none;"></a>
+	                   </div>
 	                  <ul>
-	                  {{movies}}
 	                  	<li v-for="movie in movies">
-                                <div class="movie-title">{{movie.title}}</div>
+                                <div class="movieType-title">{{movieType.title}}</div>
                                 <div class="line"></div>
-		                  	    <div class="flex-box" v-for="item in movie.subjects">
-			                    	<img :src="item.images.medium">
+		                  	    <div class="flex-box" v-for="move in movieType.subjects">
+			                    	<img :src="move.images.medium">
 			                    	<div class="flex-box-content">
-			                    		  <p>{{item.original_title}}</p>
-			                    		  <p class="text-muted small-size">{{item.year}}年/<span class="text-blue small-size">{{item.rating.average}}分</span>
-			                    		  <!-- star：{{item.rating.stars}} --></p>
-			                    		  <p class="small-size">主演：<span v-for="(cast,index) in item.casts"> 
+			                    		  <p>{{move.original_title}}</p>
+			                    		  <p class="text-muted small-size">{{move.year}}年/<span class="text-blue small-size">{{move.rating.average}}分</span>
+			                    		  <!-- star：{{move.rating.stars}} --></p>
+			                    		  <p class="small-size">主演：<span v-for="(cast,index) in move.casts"> 
 			                    		  	     <span v-if="index !=0">/</span>{{cast.name}}
 			                    		  </span>
 			                    		  </p>
 			                    		  <p>
-			                    		  	  <span v-for="genre in item.genres">
+			                    		  	  <span v-for="genre in move.genres">
 			                    		  	  	   <badge :text="genre" class="m-r-1"></badge>
 			                    		  	  </span>
 			                    		  </p>
@@ -29,15 +33,16 @@
 </template>
 <!-- <script type="text/ecmascript-6"> -->
 <script>
-import {Swiper,Badge} from 'vux'
+import {Swiper,Badge,Search} from 'vux'
 export default {
 	components: {
-	      Swiper,Badge
+	      Swiper,Badge,Search
 	},
     data () {
 	    return {
 	        movies:[],
-	        count:5//获取条数
+	        count:5,//获取条数
+	        searchText:""
 	    }
 	 },
 	mounted: function() {
@@ -48,23 +53,30 @@ export default {
      methods: {
             //获取正在热映列表
 	        async getIn_theaters() {
-	             let data = await this.$http.get(`/v2/movie/in_theaters?start=0 & count=${this.count}`);
+	             let data = await this.$http.get(`/v2/movie/in_theaters?count=${this.count}`);
 	             if (data.status == 200) {
 	             	   this.movies[0]=data.data;
 	             }
 	        },
 	        //获取即将上映列表
 	        async getComing_soon() {
-	             let data = await this.$http.get(`/v2/movie/coming_soon?start=0 & count=${this.count}`);
+	             let data = await this.$http.get(`/v2/movie/coming_soon?count=${this.count}`);
 	             if (data.status == 200) {
 	             	   this.movies[1]=data.data;
 	             }
 	        },
 	        //获取新片榜列表
 	        async getUs_box() {
-	             let data = await this.$http.get(`/v2/movie/us_box?start=0 & count=${this.count}`);
+	             let data = await this.$http.get(`/v2/movie/us_box?count=${this.count}`);
 	             if (data.status == 200) {
 	             	   this.movies[2]=data.data;
+	             }
+	        },
+	        async searchMovie(){
+	         	 console.log(this.searchText)
+	        	 let data = await this.$http.get(`/v2/movie/search?q=${this.searchText}`);
+	             if (data.status == 200) {
+	             	   this.movies=data.data;
 	             }
 	        }
 
@@ -72,5 +84,6 @@ export default {
 }
 </script>
 <style type="text/css">
+  .weui-search-bar__box{border:1px solid #eee;border-radius: 6px;}
 </style>
 
